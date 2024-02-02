6e465b741e49da9eedf177bcbef9c4749df9efb3
DhaivatCLG <155412676+DhaivatCLG@users.noreply.github.com>
2024-02-02 10:15:54 +0530

    Add files via upload

---
+++ DBConnectivity/DBConnectivity/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DBConnectivity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCreateDatabase_Click(object sender, System.EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(@"Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\DBTest.mdf;Integrated Security=True;User Instance=True");
+            cn.Open();
+            String sql = "select * from TESTTABLE where ID = '"+textBox1.Text+"' and Name = '"+textBox2.Text+"'";
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                MessageBox.Show("Welcome my friends");
+
+
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
